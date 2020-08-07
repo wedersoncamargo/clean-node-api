9216355ea6a32668a4adf0d861b91e417be6aeb8
Wederson Camargo <wederson.emilio@gmail.com>
2020-08-07 19:57:28 -0300

    feat: ensure SingUpController returns 500 if EmailValidator throws

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,6 +1,6 @@
 import { HttpResponse } from '../protocols/http'
 
-export const badRequest  = (error: Error) : HttpResponse => ({
+export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
-  body: error  
+  body: error
 })
